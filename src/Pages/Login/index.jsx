0d25096a3c48c7e555ff730e3bd9696f661d7a43
VihTech <vitoriamaria.rodrigues02@gmail.com>
2023-05-20 20:10:32 -0300

    Tela do login

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,8 +1,23 @@
+import { useState } from "react"
 import Header from "../../Components/Header"
 import { api } from "../../Services/API"
 import { loginToken, setarIdUsuario, setarNomeUsuario, setarTipoUsuario } from "../../Services/localstorage"
 import './style.css'
+import {FaEyeSlash, FaEye, FaUserAlt, FaUserCircle} from 'react-icons/fa'
+
 export const Login = () =>{
+
+    const [verSenha, setVerSenha] = useState(false)
+    const [nome, setNome] = useState('')
+    const [senha, setSenha] = useState('')
+
+    const mostrarSenha = () => {
+        if (verSenha === false){
+            setVerSenha(true)
+        }else{
+            setVerSenha(false)
+        }
+    }
 
     const fazerLogin = async() =>{
 
@@ -26,8 +41,71 @@ export const Login = () =>{
     return(
         <>
             <Header/>
-            <main>
-                <button onClick={fazerLogin}>Login</button>
+            <main className="main-logar-usuario">
+
+                <div className="main-logar-usuario-bolinha-um"></div>
+                <div className="main-logar-usuario-bolinha-dois"></div>
+                <div className="main-logar-usuario-coonteiner">
+
+                    <div className="main-logar-usuario-conteiner-texto">
+                        <div className="main-logar-usuario-conteiner-texto-titulo">
+                            <h2>Se você deseja criar uma conta, entre em contato com o administrador responsável.</h2>
+                        </div>
+                        <div className="main-logar-usuario-conteiner-texto-imagem"></div>
+                    </div>
+                    <div className="main-logar-usuario-conteiner-formulario">
+                        <div className="main-logar-usuario-conteiner-formulario-conteiner">
+                            <div className="main-logar-usuario-conteiner-formulario-conteiner-login">
+                                <h2>Login</h2>
+                            </div>
+
+                            <div className="main-logar-usuario-conteiner-formulario-conteiner-conteiner">
+                                <FaUserCircle/>
+                            </div>
+
+                            <div className="main-logar-usuario-conteiner-formulario-conteiner-informacoes">
+
+                                    <p>Nome</p>
+                                    <div className="main-logar-usuario-conteiner-formulario-conteiner-informacoes-input">
+                                        <div className="main-logar-usuario-conteiner-formulario-conteiner-informacoes-input-svg"><FaUserAlt/></div>
+                                        
+                                        <input type="text"  onChange={(e) => setNome(e.target.value)}/>
+                                    </div>
+
+                                    <p>Senha</p>
+                                    <div className="main-logar-usuario-conteiner-formulario-conteiner-informacoes-input">
+
+                                        {verSenha === false? (
+                                            <>
+                                                <div className="main-logar-usuario-conteiner-formulario-conteiner-informacoes-input-svg" onClick={mostrarSenha}><FaEyeSlash/></div>
+                                            
+                                                <input type="password" onChange={(e) => setSenha(e.target.value)}/>
+                                            </>
+
+                                        ): (
+                                            <>
+                                                <div className="main-logar-usuario-conteiner-formulario-conteiner-informacoes-input-svg" onClick={mostrarSenha}><FaEye/></div>
+                                            
+                                                <input type="text" onChange={(e) => setSenha(e.target.value)}/>
+                                            </>
+                                        )}
+                                        
+                                    </div>
+
+                            </div>
+
+                            <div className="main-logar-usuario-conteiner-formulario-conteiner-botao">
+                                <button onClick={fazerLogin}>Logar</button>
+                            </div>
+
+                            <div className="main-logar-usuario-conteiner-formulario-conteiner-msg">
+                                        
+                            </div>
+    
+                        </div>
+                    </div>
+
+                </div>
             </main>
         </>
     )
--- src/Services/API/index.jsx
+++ src/Services/API/index.jsx
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export const api = axios.create({
-    baseURL: "https://repositorio-capitrano-api.vercel.app/"
+    baseURL: "https://repositorio-capistrano-api.vercel.app/"
 })
