5109893c9f646d2481d3b301ec05ebd532172ae5
leiaifce <leia@maranguape.ifce.edu.br>
2023-05-20 13:49:23 -0300

    Cadastrando obras

--- src/Pages/Cadastrar Obras/index.jsx
+++ src/Pages/Cadastrar Obras/index.jsx
@@ -1,7 +1,31 @@
+import { useState } from "react"
 import Header from "../../Components/Header"
 import './style.css'
+import { api } from "../../Services/API"
 
 export const CadastrarObras = () => {
+
+    const [titulo, setTitulo] = useState('')
+    const [autores, setAutores] = useState('')
+    const [resumo, setResumo] = useState('')
+    const [descricao, setDescricao] = useState('')
+
+    const adicionarObras = async () => {
+        const data = {
+            titulo,
+            autores,
+            descricao,
+            resumo
+        }
+
+        try{
+            const res = await api.post('/cadastro_obras', data)
+            console.log(res)
+        }catch(err){
+            console.log(err)
+        }
+    }
+
     return(
         <div>
             <Header/>
@@ -13,30 +37,31 @@ export const CadastrarObras = () => {
                     <div className="main-cadastrar-obras-formulario-titulo">
                         <div className="main-cadastrar-obras-formulario-titulo-titulo">
                             <h2>Titulo:</h2>
-                            <input type="text" />
+                            <input type="text" onChange={(e) => setTitulo(e.target.value)}/>
                         </div>
 
                         <div className="main-cadastrar-obras-formulario-titulo-autores">
                             <h2>Autores:</h2>
-                            <input type="text" />
+                            <input type="text" onChange={(e) => setAutores(e.target.value)}/>
                         </div>
                     </div>
                     <div className="main-cadastrar-obras-formulario-resumo">
                         <div className="main-cadastrar-obras-formulario-resumo-divisao">
                             <h3>Resumo:</h3>
-                            <input type="text"/>
+                            <textarea type="text" wrap="soft" onChange={(e) => setResumo(e.target.value)}/>
+
                         </div>
                     </div>
                     <div className="main-cadastrar-obras-formulario-descricao">
                         <div className="main-cadastrar-obras-formulario-descricao-divisao">
                             <h3>Descrição:</h3>
-                            <input type="text" name="" id="" />
+                            <textarea type="text" wrap="soft" onChange={(e) => setDescricao(e.target.value)}/>
                         </div>
                     </div>
                 </div>
 
                 <div className="main-cadastrar-obras-botao">
-                    <button>Publicar</button>
+                    <button onClick={adicionarObras}>Publicar</button>
                 </div>
 
             </main>
